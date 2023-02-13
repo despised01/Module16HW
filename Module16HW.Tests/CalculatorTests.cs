4a4e1c5aff19a699f2e18d6119709b6f967396a5
Антон Левин <despised01@gmail.com>
2023-02-13 21:42:07 +0300

    Добавьте файлы проекта.

---
+++ Module16HW.Tests/CalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Module16HW.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Additional(10, 15) == 25);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Subtraction(40, 20) == 20);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Multiplication(5, 6) == 30);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(calculator.Division(30, 10) == 3);
+        }
+
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+    }
+}
